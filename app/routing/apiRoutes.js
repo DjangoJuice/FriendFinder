e51fcdd23dac9af5d8f079298dd491c93b8ac7d5
DjangoJuice <dungeonmesher@gmail.com>
2018-09-10 21:15:21 -0400

    Survey 2.0 + Routes enabled

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,17 @@
+var path = require("path");
+
+
 // Routes
 // =============================================================
 
-// 
+
+module.exports = function(app) {
+
 app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "friends.js"));
+    res.sendFile(path.join(__dirname, "../data/friends.js"));
   });
   
 app.post("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "friends.js"));
+    res.sendFile(path.join(__dirname, "../data/friends.js"));
   });
+};
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
 // Sets up the Express App
 // =============================================================
@@ -13,6 +12,10 @@ var PORT = process.env.port || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
   
   // Displays all characters
