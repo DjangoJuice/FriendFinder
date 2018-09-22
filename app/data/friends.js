cb76023675ce8f1893faded9aa120d6b0c49e1f1
DjangoJuice <dungeonmesher@gmail.com>
2018-09-22 19:14:44 -0400

    Fixed the Friend Matching Function

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,71 @@
 var friendsArray = [
-
+    
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Bob",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            4,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        "name":"Sally",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            2,
+            1,
+            2,
+            4,
+            2,
+            1,
+            2,
+            2,
+            2,
+            1
+          ]
+      },
+      {
+        "name":"Alvaro",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      }
+       
+   
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,6 +4,7 @@
 var path = require("path");
 var friendsArray = require("../data/friends");
 
+//console.log(friendsArray[0].q1);
 
 // Routes
 // =============================================================
@@ -13,12 +14,66 @@ module.exports = function(app) {
 
 app.get("/api/friends", function(req, res) {
     res.json(require("../data/friends"));
-    //res.json(path.join(__dirname, "../data/friends.js"));
   });
   
 app.post("/api/friends", function(req, res) {
-    friendsArray.push(req.body);
-    res.json(true);
-    //res.json(require(path.join(__dirname, "../data/friends.js")));
+    //friendsArray.push(req.body);\
+
+    var newFriend = req.body;
+  
+
+     findBestMatch(newFriend, function(matchData){
+      res.json(matchData);
+     })
+    
+    // for (var friend in friendsArray) {
+    //   res.json(req.body.q1 - friendsArray[friend].q1)
+    // }
+
+    //Find the best match here from friendsArray
+    // for (var friend in friendsArray) {
+    //   for (i=0; i < 9; i++) {
+    //   (friendsArray[friend].qi - req.body.qi)
+    //   }
+    // }
+
+  
+
+    //res.json(friendsArray[0]);
   });
 };
+
+function findBestMatch(newFriend, callback) {
+
+  var bestMatch;
+  var bestMatchDifference;
+
+  friendsArray.forEach(ele => {
+      var totalDifference = 0;
+     
+      ele.scores.forEach((e, i)=> {
+        var scoreDifference = e - newFriend.scores[i];
+        totalDifference += Math.abs(scoreDifference);
+      });
+
+     
+
+      if( bestMatchDifference ) {
+          if(totalDifference < bestMatchDifference) {
+            bestMatchDifference = totalDifference;
+            bestMatch = ele;
+          }
+      } else {
+        bestMatchDifference = totalDifference;
+        bestMatch = ele;
+      }
+
+      console.log("This is the total difference " +totalDifference + "for " + ele.name );
+
+  });
+
+  console.log("This is the best match " + bestMatch.name + "And this is the total difference " + bestMatchDifference);
+
+  callback(bestMatch);
+
+}
